45a94105943090e4276b6ae6fef6d2bc10269154
Eric McGregor <eric@n0code.net>
2023-03-15 21:07:40 -0400

    routing demo

---
+++ app19/assets/AboutView-ffff0fe0.js
@@ -0,0 +1,1 @@
+import{o as t,c as s,a as n,b as r,t as c,F as l,_ as u}from"./index-8601788b.js";const p={__name:"AboutView",props:["main"],setup(a){const e=a;return e.main.style.backgroundColor="#93d7dc",(o,m)=>(t(),s(l,null,[n(u,{label:"About"}),r("div",null," About "+c(o.$route.name)+"... ",1)],64))}};export{p as default};
